843e5afa96076b0addce22318d91965730f9a2c3
Garrett Koller <gkoller@catalist.us>
2021-02-14 14:22:04 -0500

    Patch: Disabled read/write features of PDFTron WebViewer and added documentation URLs in comments

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,22 @@ function App() {
   const viewerDiv = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
+    /* Using PDFTron WebViewer for React
+    // Documentation: https://www.pdftron.com/documentation/web/get-started/react/
+    // Tutorial: https://www.youtube.com/watch?v=bVhWXuLSL0k  */
     WebViewer({
-        path: 'lib',
-        initialDoc: 'webviewer-demo.pdf'
-      }, viewerDiv.current as HTMLDivElement).then(instance => {
+      path: 'lib',
+      initialDoc: 'webviewer-demo.pdf',
+      accessibleMode: true,
+      enableAnnotations: false,
+      enableMeasurement: false,
+      enableRedaction: false,
+      isAdminUser: false,
+      isReadOnly: true,
+      //disabledElements: [
+      //  'viewControlsButton'
+      //],
+    }, viewerDiv.current as HTMLDivElement).then(instance => {
 
     })
   }, [])
@@ -18,7 +30,7 @@ function App() {
     <div className="App">
       <div className='webviewer' ref={viewerDiv} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
