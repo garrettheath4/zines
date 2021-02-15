fc249ad317c0f65bd6b28b801c13e69e682aa498
Garrett Koller <gkoller@catalist.us>
2021-02-14 23:08:29 -0500

    BROKEN: Trying to convert flipbook.js example code from PDFTron and insert into example React code from PDFTron

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useRef, useEffect } from 'react'
-import WebViewer from '@pdftron/webviewer'
-import './App.css'
 import $ from 'jquery'
+import { CoreControls } from '@pdftron/webviewer'
+import 'turn.js'
+import './App.css'
 
 function App() {
   //TODO: Add turn.js (manually or using PDFTron's wrapper script `flipbook.js`
@@ -9,52 +10,99 @@ function App() {
   //   OR use <Turn> React component I borrowed from the below URL
   //        https://codesandbox.io/s/pmkq9zvwkx?file=/src/index.js
 
-  const options = {
-    width: 800,
-    height: 600,
-    autoCenter: true,
-    display: 'double',
-    acceleration: true,
-    elevation: 50,
-    gradients: !$.isTouch,
-    when: {
-      turned: function(e, page) {
-        console.log('Current view: ', $(this).turn('view'))
-      }
-    }
-  }
+  //CoreControls.setWorkerPath('lib/core')
 
-  const pages = [
-    //TODO
-  ]
+  const source = 'webviewer-demo.pdf'
 
-  const viewerDiv = useRef<HTMLDivElement>(null)
+  const flipbookElement = useRef<HTMLDivElement>(null)
+  const loadingMessageElement = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     /* Using PDFTron WebViewer for React
     // Demo: https://www.pdftron.com/webviewer/demo/online-flipbook
     // Documentation: https://www.pdftron.com/documentation/web/get-started/react/
     // Tutorial: https://www.youtube.com/watch?v=bVhWXuLSL0k  */
-    WebViewer({
-      path: 'lib',
-      initialDoc: 'webviewer-demo.pdf',
-      accessibleMode: true,
-      enableAnnotations: false,
-      enableMeasurement: false,
-      enableRedaction: false,
-      isAdminUser: false,
-      isReadOnly: true,
-      //disabledElements: [
-      //  'viewControlsButton'
-      //],
-    }, viewerDiv.current as HTMLDivElement).then(instance => {
+    const documentPromise: Promise<CoreControls.Document> = CoreControls.createDocument(source)
 
+    documentPromise.then(doc => {
+      const info = doc.getPageInfo(1);
+      const width = info.width;
+      const height = info.height;
+      const pageCount = doc.getPageCount();
+      const promises = [];
+      const canvases: { pageNum: number, canvas: HTMLCanvasElement }[] = [];
+
+      // @ts-ignore
+      const boundingRect = flipbookElement.current.getBoundingClientRect();
+      let flipbookHeight = boundingRect.height;
+      let flipbookWidth = boundingRect.width;
+      if (((flipbookHeight * width) / height) * 2 < flipbookWidth) {
+        flipbookWidth = ((flipbookHeight * width) / height) * 2;
+      } else {
+        flipbookHeight = ((flipbookWidth / width) * height) / 2;
+      }
+
+      for (let i = 0; i < pageCount; i++) {
+        promises.push(
+          /* eslint-disable-next-line no-loop-func */
+          new Promise(resolve => {
+            // Load page canvas
+            const pageNumber = i + 1;
+            return doc.requirePage(pageNumber).then(() => {
+              return doc.loadCanvasAsync({
+                pageNumber,
+                drawComplete: (canvas, pageNum) => {
+                  canvases.push({ pageNum, canvas });
+
+                  // @ts-ignore
+                  loadingMessageElement.current.innerHTML = `Loading page canvas... (${canvases.length}/${pageCount})`;
+                  resolve(null);
+                },
+                // @ts-ignore
+                isInternalRender: true,
+              });
+            });
+          })
+        );
+      }
+
+      Promise.all(promises).then(() => {
+        // @ts-ignore
+        flipbookElement.current.removeChild(loadingMessageElement.current);
+        // @ts-ignore
+        flipbookElement.current.style.margin = '60px 0px 0px auto';
+
+        // @ts-ignore
+        canvases.sort((a, b) => a.pageNum - b.pageNum).forEach(o => flipbookElement.current.appendChild(o.canvas));
+
+        // @ts-ignore
+        $('#flipbook').turn({
+          width: flipbookWidth,
+          height: flipbookHeight,
+        });
+
+        /*
+        setTimeout(() => {
+          $('#flipbook').turn('next');
+        }, 500);
+
+        document.getElementById('previous').onclick = () => {
+          $('#flipbook').turn('previous');
+        };
+
+        document.getElementById('next').onclick = () => {
+          $('#flipbook').turn('next');
+        };
+         */
+      });
     })
   }, [])
 
   return (
     <div className="App">
-      <div className='webviewer' ref={viewerDiv} />
+      <div id='flipbook' ref={flipbookElement}>
+        <div id='loading-message' ref={loadingMessageElement} />
+      </div>
     </div>
   )
 }
