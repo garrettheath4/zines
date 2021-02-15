c6d65a068951d716df170e3e77590eb794281750
Garrett Koller <gkoller@catalist.us>
2021-02-15 00:57:00 -0500

    Minor: Added publication date and location to PDF metadata file

--- src/pdfs.ts
+++ src/pdfs.ts
@@ -1,10 +1,34 @@
 interface PdfMetadata {
   filePath: string,
   sourceUrl: string,
+  publishDate?: {
+    decade?: number,
+    year?: number,
+    month?: number,
+    day?: number,
+  },
+  publishLocation?: {
+    country?: string,
+    state?: string,
+    city?: string,
+  },
 }
 
 const pdfs: PdfMetadata[] = [
-  { filePath: 'QAW_Download_Library_Jan26_2021/gayliberator.pdf', sourceUrl: 'https://queer.archive.work/library/download/index.html' }
+  {
+    filePath: 'QAW_Download_Library_Jan26_2021/gayliberator.pdf',
+    sourceUrl: 'https://queer.archive.work/library/download/index.html',
+    publishDate: {
+      decade: 1970,
+      year: 1971,
+      month: 9,
+    },
+    publishLocation: {
+      country: 'USA',
+      state: 'MI',
+      city: 'Detroit',
+    },
+  },
 ]
 
 
