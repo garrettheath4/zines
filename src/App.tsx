422b6516cbb5f3f727a6e31fb5204cff80df5796
Garrett Koller <gkoller@catalist.us>
2021-02-15 13:33:11 -0500

    Patch: Extracted PDFTron web page plugin into its own React component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
 import React, { useRef, useEffect } from 'react'
-import WebViewer, { WebViewerInstance } from '@pdftron/webviewer'
-import pdfs from './pdfs'
 import './App.css'
+import PdfTron from './PdfTron'
 
 function App() {
-  const viewerDiv = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    /* Using PDFTron WebViewer for React
-    // Documentation: https://www.pdftron.com/documentation/web/get-started/react/
-    // Tutorial: https://www.youtube.com/watch?v=bVhWXuLSL0k  */
-    WebViewer({
-      path: 'lib',
-      initialDoc: `pdfs/${pdfs[0].filePath}`,
-      accessibleMode: true,
-      enableAnnotations: false,
-      enableMeasurement: false,
-      enableRedaction: false,
-      isAdminUser: false,
-      isReadOnly: true,
-    }, viewerDiv.current as HTMLDivElement).then((instance: WebViewerInstance) => {
-      instance.setToolMode('Pan')
-      instance.disableTools(['AnnotationEdit'])
-    })
-  }, [])
-
   return (
     <div className="App">
-      <div className='webviewer' ref={viewerDiv} />
+      <PdfTron />
     </div>
   )
 }
---
+++ src/PdfTron.tsx
@@ -0,0 +1,33 @@
+import React, { useRef, useEffect } from 'react'
+import WebViewer, { WebViewerInstance } from '@pdftron/webviewer'
+import pdfs from './pdfs'
+import './App.css'
+
+function PdfTron() {
+  const viewerDiv = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    /* Using PDFTron WebViewer for React
+    // Documentation: https://www.pdftron.com/documentation/web/get-started/react/
+    // Tutorial: https://www.youtube.com/watch?v=bVhWXuLSL0k  */
+    WebViewer({
+      path: 'lib',
+      initialDoc: `pdfs/${pdfs[0].filePath}`,
+      accessibleMode: true,
+      enableAnnotations: false,
+      enableMeasurement: false,
+      enableRedaction: false,
+      isAdminUser: false,
+      isReadOnly: true,
+    }, viewerDiv.current as HTMLDivElement).then((instance: WebViewerInstance) => {
+      instance.setToolMode('Pan')
+      instance.disableTools(['AnnotationEdit'])
+    })
+  }, [])
+
+  return (
+    <div className='webviewer' ref={viewerDiv} />
+  )
+}
+
+export default PdfTron
